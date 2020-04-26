dbc3230175fd71665ef61fa10c0acba403d70aeb
vladik-cmd <63416337+vladik-cmd@users.noreply.github.com>
2020-04-26 10:52:02 +0300

    MartaUp

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+	$('.header-slider').slick({
+		arrows: false,
+		vertical: true,
+		verticalSwiping: true,
+		dots: true,
+		dotsClass: 'header-dots',
+		autoplay: 3000,
+	});
+
+});
