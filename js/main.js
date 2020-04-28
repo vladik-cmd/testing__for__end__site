fd8a268065739f6655f3832e92909bd582316db6
vladik-cmd <63416337+vladik-cmd@users.noreply.github.com>
2020-04-28 22:06:04 +0300

    Адаптив начало (header)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(function(){
 
+	$('.header-btn').on('click', function(){
+		$('.header-top__menu').addClass('active');
+	});
+
+	$('.close-btn').on('click', function() {
+		$('.header-top__menu').removeClass('active')
+	});
+
 });
