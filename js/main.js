4c041d3a78e36bbb166e31726d63878b1ecd9471
vladik-cmd <63416337+vladik-cmd@users.noreply.github.com>
2020-06-03 21:45:59 +0300

    Bot-Creators
    test Bot-Creators

--- js/main.js
+++ js/main.js
@@ -1,11 +1,45 @@
 $(function(){
+    $("#scroll").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
 
-	$('.header-btn').on('click', function(){
-		$('.header-top__menu').addClass('active');
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+    $("#btnup").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+    $('.btn').on('click', function(){
+		$('.popup').addClass('active');
 	});
 
-	$('.close-btn').on('click', function() {
-		$('.header-top__menu').removeClass('active')
+	$('.popup-btn').on('click', function(){
+		$('.popup').removeClass('active');
 	});
 
+	$('.popup-overlay').on('click', function(){
+		$('.popup').removeClass('active');
+	});
+
+	$('.more-btn').on('click', function(){
+		$('.skills-item').toggleClass('active');
+	});
+
+	$('.more-btn').on('click', function(){
+		$('.more-btn').toggleClass('active');
+	});
+	
 });
+
+
